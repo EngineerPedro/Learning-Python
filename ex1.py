bad42ffe998a05754d2181d58e9285358b983920
EngineerPedro <plguzman1993@gmail.com>
2019-03-31 21:55:46 -0700

    Just added some basic comments to the file for clarity, purpose and documentaton

--- ex1.py
+++ ex1.py
@@ -1,3 +1,8 @@
+#Pedro Lebron Guzman III
+#Learning Python the Hard Way
+#March 31 2019
+#PURPOSE: we learn how to get basic things printed to the screne
+#Lesson Program #1
 print("HelloWorld")
 print("Hello again")
 print("I Like typing this.")
