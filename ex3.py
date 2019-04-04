d9dfce9598cd52e3b47808af031aa9ed51d284a9
EngineerPedro <plguzman1993@gmail.com>
2019-04-04 01:36:22 -0700

    Numbers and symbols

---
+++ ex3.py
@@ -0,0 +1,37 @@
+#Pedro Lebron Guzman III
+#Learning Python the Hard Way
+#March 31 2019
+#PURPOSE: Numbers and Math Symbols
+#Lesson Program #3
+
+#message to notify user
+print("I will now count my chickens: ")
+
+#demonstration of math
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 -25 * 3 % 4)
+
+#prompt
+print("Now i'll count some eggs")
+
+#operation
+print(3+2+1-5+4%2-1/4+6)
+
+#question
+print("Is it true that 3 + 2 < 5 - 7?")
+#answer
+print(3 + 2 < 5 - 7)
+
+#more fun practicing: this time combining prompts with operations
+print("What is  3 + 2?", 3 + 2)
+print("What is  5 - 7?", 5 - 7)
+
+#prompt
+print("Oh thats why its False.")
+
+#it never stops
+print("How about some more")
+
+print("Is it Greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal", 5 <= -2)
