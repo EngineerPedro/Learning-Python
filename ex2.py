b3b157f7dc651bb3fe50bc9a5d6e0b94780d4e3b
EngineerPedro <plguzman1993@gmail.com>
2019-04-04 00:51:14 -0700

    In depth commenting excercise

---
+++ ex2.py
@@ -0,0 +1,14 @@
+#Pedro Lebron Guzman III
+#Learning Python the Hard Way
+#April 4  2019
+#PURPOSE: commenting
+#Lesson Program #2
+
+# A comment , this is so you can read the program late.
+#Anything after the # sign is ignored by python
+print("I could have code like this")#and the comment after is ignored
+
+#You can also use a comment to "disable" or comment out code
+#print ("This wont run")
+
+print("This will run.")
