8289daa3ad7c85e95212cf4a20ab07e0b8a1d0cd
EngineerPedro <plguzman1993@gmail.com>
2019-04-05 00:07:29 -0700

    Printing in depth for more practice

---
+++ ex8.py
@@ -0,0 +1,19 @@
+#Pedro Lebron Guzman III
+#Learning Python the Hard Way
+#April 6  2019
+#PURPOSE: Printing Printing
+#Lesson Program #8
+
+
+formatter = "{} {} {} {}"
+
+print(formatter.format(1,2,3,4))
+print(formatter.format("one","two","three","four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter,formatter,formatter,formatter))
+print(formatter.format(
+        "Try your",
+        "Own test here",
+        "Maybe a poem",
+        "Or a song about fear"
+))
