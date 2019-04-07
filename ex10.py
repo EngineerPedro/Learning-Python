56726496c73a2eb13c9083df1ed50b273b3a570b
EngineerPedro <plguzman1993@gmail.com>
2019-04-06 18:27:24 -0700

    "\" operations in practical use

---
+++ ex10.py
@@ -0,0 +1,21 @@
+#Pedro Lebron Guzman III
+#Learning Python the Hard Way
+#April 6  2019
+#PURPOSE: utilization of the \ character in strings
+#Lesson Program #10
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \non a line"
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
