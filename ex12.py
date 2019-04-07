3e92068cebfc2f845d782689a7deb803c5c8f9e0
EngineerPedro <plguzman1993@gmail.com>
2019-04-06 18:56:33 -0700

    Learned a different more efficient way to prompt user for input

---
+++ ex12.py
@@ -0,0 +1,11 @@
+#Pedro Lebron Guzman III
+#Learning Python the Hard Way
+#April 6  2019
+#PURPOSE: More practice prompting people for input
+#Lesson Program #12
+
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+print(f"So, you're {age} years old ,{height} inches tall and weigh {weight} pounds.")
