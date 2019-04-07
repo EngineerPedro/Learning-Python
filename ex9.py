16686fc25779b3bd66bbcd8f71cdb4f0319ddf6f
EngineerPedro <plguzman1993@gmail.com>
2019-04-06 17:43:04 -0700

    Practicing printing to the screen

---
+++ ex9.py
@@ -0,0 +1,20 @@
+#Pedro Lebron Guzman III
+#Learning Python the Hard Way
+#April 6  2019
+#PURPOSE: Printing Printing Printing
+#Lesson Program #9
+
+#new strange stuff
+
+days = "Mon Tues Wed Thur Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+Theres something going on here.
+With the three double-quotes
+We'll be able to type as much as we like.
+Even 4 Lines we want, or  5, or 6.
+""")
