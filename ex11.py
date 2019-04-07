d48dd270abd4d181efdd3a4200dd670d2776fa2c
EngineerPedro <plguzman1993@gmail.com>
2019-04-06 18:45:52 -0700

    Learn to ask questions that prompt the user for input

---
+++ ex11.py
@@ -0,0 +1,18 @@
+#Pedro Lebron Guzman III
+#Learning Python the Hard Way
+#April 6  2019
+#PURPOSE: asking questions that will prompt the user
+#Lesson Program #11
+
+
+#questions that lead to prompting the user for input
+print("How old are you?", end= ' ')
+age = input()#prompt
+
+print("How tall are you?", end=' ')
+height = input()#prompt
+
+print("How much do you weigh?",end= ' ')
+weight = input()#prompt
+
+print(f"So, you're {age} years old ,{height} inches tall and weigh {weight} pounds")
